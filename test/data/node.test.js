420a03a1b897c4f17c9236914f58b7ee0c80ebb6
Emile Shodiev <emile.shodiev@yandex.ru>
2018-11-26 13:44:03 +0300

    test templates

---
+++ test/data/node.test.js
@@ -0,0 +1,14 @@
+var Node = require('../../src/data/node');
+
+
+describe("Node", function () {
+   
+    describe("getLinkTo", function () {
+        it("should return valid Link instance if given Node instance") 
+
+        it("should return valid Link instance if given Node id") 
+
+        it("should return null when link to given node does not exist") 
+    });
+
+});
--- test/data/nodeset.test.js
+++ test/data/nodeset.test.js
@@ -9,16 +9,42 @@ describe("NodeSet", function () {
 
     describe("addNode", function () {
         
-        it("should throw when trying to add new node with id of other node in the set")
+        it("should throw when trying to add new node with id of other node in the set");
 
-        it("should add node to nodes array")
+        it("should add node to nodes array");
 
-        it("should add node to nodes map")
+        it("should add node to nodes map");
 
-        it("should set nodeset property of the node")
+        it("should set nodeset property of the node");
 
-        it("should return new node (instance of Node)")
+        it("should return new node (instance of Node)");
 
-    })
+    });
+
+    describe("removeNode", function () {
+        it("should remove node from NodeSet.nodes array and NodeSet.nodeMap object");
+
+        it("should remove node when given Node instance");
+
+        it("should remove node when given Node id");
+    });
+
+    describe("addLink", function () {
+        it("shouldn't add new link between already linked nodes");
+        
+        it("should add link to NodeSet.links, node0.links and node1.links arrays");
+    });
+
+    describe("removeLink", function () {
+        it("should remove link from NodeSet.links, link.node0.links and link.node1.links arrays");
+    });
+
+    describe("getLinkBetween", function () {
+        it("should return link when given two node instances");
+            
+        it("should return link when given two node ids (as numbers)");
+
+        it("should return link when given node instance and node id (in any order)");
+    });
 
 })
